0fabe6c1079d7d07a1f96ddd958a1c04a1ee2c32
fengli <xiyouji.xiaoyaoyou.im186@gmail.com>
2021-03-15 10:53:48 +0800

    cronet仓库测试OK

---
+++ imcronetlib/src/main/java/com/leimans/lib/imcronetlib/TestCronet.java
@@ -0,0 +1,12 @@
+package com.leimans.lib.imcronetlib;
+
+import org.chromium.net.CronetEngine;
+
+/**
+ * @Author ：FengLi
+ * @Date : 2021-03-12
+ * @Description :
+ */
+public class TestCronet {
+    CronetEngine cronetEngine;
+}
